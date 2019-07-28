fc5115ab923a88e7464c737b6d38a780be18159a
mearinne <32369607+mearinne@users.noreply.github.com>
2019-07-28 22:40:40 +0200

    ChangedParentFolderOfProject

---
+++ F_bio/BioFighter/Assets/Scripts/shooting.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooting : MonoBehaviour
+{
+   public Transform firepoint;
+    LineRenderer laser;
+
+    private void Start()
+    {
+        laser = gameObject.GetComponent<LineRenderer>();
+        laser.enabled = false; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       /* if (Input.GetButtonDown("Fire1"))
+        {
+            StopCoroutine("shoot");
+            StartCoroutine("shoot");
+        }*/
+
+    }
+
+    public IEnumerator shoot()
+    {
+        while (Input.GetButton("Fire1"))
+        {
+            laser.enabled = true;
+            Ray ray = new Ray(transform.position, transform.right);
+            RaycastHit rayHit;
+            laser.SetPosition(0,ray.origin);
+            if(Physics.Raycast(ray, out rayHit, 100))
+            {
+                laser.SetPosition(1, rayHit.point);
+            }
+            else
+            {
+                laser.SetPosition(1, ray.GetPoint(100));
+            }
+
+
+            yield return null;
+        }
+        laser.enabled = false;
+    }
+}
